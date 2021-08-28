fb91e78bcab57529993c08b651b4956d6e1788ab
Zach <zachary.bretz@gmail.com>
2021-08-28 11:48:20 -0700

    Centered logo vertically and horizontally on load page

--- client/pages/loading.js
+++ client/pages/loading.js
@@ -1,14 +1,26 @@
 import styles from '../styles/LoadingPage.module.css'
+import Head from 'next/head'
+
 // logo animation (5s) => kick to index
 // router.push('/login')
 
 export default function LoadingPage() {
   return (
-    <div>
-      <div>
-        This is Loading Page
-        Lorem Ipsum
+    <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
+      <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display&display=swap" rel="stylesheet"></link>
+    </Head>
+
+    <div id={styles.container}>
+      <div id='u-wrapper'>
+        <p id='u'>U</p>
+      </div>
+      <div id='w-container'>
+        <p id='w'>W</p>
       </div>
     </div>
+    </>
   )
 }
