961e18057d84947abe7d4ad94198097c1d15e90c
Brian <blee0613@gmail.com>
2021-08-28 14:03:36 -0700

    redid CSS dashboard

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -1,6 +1,8 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import Image from 'next/image';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import NavBar from '../components/NavBar';
 import styles from '../styles/Dashboard.module.css';
 
@@ -9,69 +11,76 @@ export default function Dashboard() {
     <div>
       <NavBar />
       <div className={styles.container}>
-        <div className={styles.left}>
-          <div className={styles.greeting}>
-            Welcome Zariopheef!
+        <div className={styles.topBox}>
+          <div className={styles.left}>
+            <div>
+              <div className={styles.quote}>
+
+                <span>
+                  We believe in healthcare for everyone. For us, everyone really does
+                  include every person and identity. Our clinics are inclusive, judgment-free zones.
+                </span>
+
+              </div>
+            </div>
           </div>
-          <form>
-            <input
-              placeholder="Search"
-              className={styles.search}
-            />
-          </form>
+          <div className={styles.right}>
+            {/* <div className={styles.dashboardTitle}>
+              User Dashboard
+            </div> */}
+            <div className={styles.topRight}>
+              <div className={styles.greeting}>
+                Welcome Zariopheef!
+              </div>
+              <form>
+                <input
+                  placeholder="Search"
+                  className={styles.search}
+                />
+              </form>
+            </div>
 
-          <div>
-            <div className={styles.quote}>
+            <div className={styles.yourTeam}>
+              <div className={styles.yourTeamTitle}>
+                Your Preferred Providers
+              </div>
+              <div className={styles.providerCardContainer}>
+                <div className={styles.providerCard}>
+                  <div className={styles.providerBarTitle}>Gender Affirming Care</div>
+                  {/* <div className={styles.providerBarDoctor}>Select Your Physician</div> */}
 
-              <span>
-                We believe in healthcare for everyone. For us, everyone reall does
-                include every person and identity. Our clinics are inclusive, judgment-free zones.
-              </span>
+                </div>
+                <div className={styles.providerCard}>
+                  <div className={styles.providerBarTitle}>Mental Well Being</div>
+                  {/* <div className={styles.providerBarDoctor}>YouBeen Jung, MD</div> */}
+                </div>
+                <div className={styles.providerCard}>
+                  <div className={styles.providerBarTitle}>Meditation Center</div>
+                  {/* <div className={styles.providerBarDoctor}>YouBeen Jung, MD</div> */}
+                </div>
+              </div>
 
             </div>
+
           </div>
         </div>
-        <div className={styles.right}>
-          <div className={styles.dashboardTitle}>
-            User Dashboard
-          </div>
-          <div className={styles.yourTeam}>
-            <div className={styles.yourTeamTitle}>
-              Your Team
-            </div>
-            <div className={styles.providerBarContainer}>
-              <div className={styles.providerBar}>
-                <div className={styles.providerBarTitle}>Gender Affirming Care</div>
-                <div className={styles.providerBarDoctor}>Select Your Physician</div>
-                <div className={styles.providerBarMenu}>|||</div>
-              </div>
-              <div className={styles.providerBar}>
-                <div className={styles.providerBarTitle}>Mental Well Being</div>
-                <div className={styles.providerBarDoctor}>YouBeen Jung, MD</div>
-                <div className={styles.providerBarMenu}>|||</div>
-              </div>
-            </div>
-
-          </div>
+        <div className={styles.bottomBox}>
           <div className={styles.yourServices}>
             <div className={styles.yourTeamTitle}>
-              Your Services
+              Discover More
             </div>
-            <div className={styles.providerBarContainer}>
-              <div className={styles.providerBar}>
-                <div className={styles.providerBarTitle}>PLACEHOLDER</div>
-                <div className={styles.providerBarDoctor}>PLACEHOLDER</div>
-                <div className={styles.providerBarMenu}>|||</div>
-              </div>
-              <div className={styles.providerBar}>
-                <div className={styles.providerBarTitle}>PLACEHOLDER</div>
-                <div className={styles.providerBarDoctor}>PLACEHOLDER</div>
-                <div className={styles.providerBarMenu}>|||</div>
-              </div>
+            <div className={styles.carousel}>
+              <ArrowBackIosIcon onClick={() => console.log('LEFT YALL')} />
+              <button type="button" className={styles.discoverButton}>Accupuncture</button>
+              <button type="button" className={styles.discoverButton}>Behavioral Health</button>
+              <button type="button" className={styles.discoverButton}>Herbal Healing</button>
+              <button type="button" className={styles.discoverButton}>Eastern Remedies</button>
+              <ArrowForwardIosIcon onClick={() => console.log('RIGHT YALL')} />
             </div>
           </div>
         </div>
       </div>
+
     </div>
   );
 }
