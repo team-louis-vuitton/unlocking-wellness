738f19220a81ed766adb4f2340d1cc7d17175495
Zach <zachary.bretz@gmail.com>
2021-08-28 10:56:38 -0700

    Initialized loading page

--- client/pages/loading.js
+++ client/pages/loading.js
@@ -1,2 +1,14 @@
+import styles from '../styles/LoadingPage.module.css'
 // logo animation (5s) => kick to index
 // router.push('/login')
+
+export default function LoadingPage() {
+  return (
+    <div>
+      <div>
+        This is Loading Page
+        Lorem Ipsum
+      </div>
+    </div>
+  )
+}
