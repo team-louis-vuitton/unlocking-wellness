93f63b66fb26844d3fc136950b8cbf6c43148261
Zadok Kim <zadokjhkim@gmail.com>
2021-08-28 16:52:09 -0700

    registration mvp completed

--- client/pages/registration.js
+++ client/pages/registration.js
@@ -3,143 +3,184 @@ import { useRouter } from 'next/router';
 
 import { useUser } from '../components/UserContext';
 
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import css from '../styles/Registration.module.css';
 
-function Copyright() {
+const Registration = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+  const [error, setError] = useState(null);
+
+  const { createUserWithEmailAndPassword } = useUser();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setError(null);
+    createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        console.log(`success. ${authUser} created in Firebase`)
+        router.push('/dashboard');
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+    <div>
+      <form className={css.main} onSubmit={onSubmit}>
+        Sign Up
+        {/* <input type='text' placeholder='Username' className={css.input}/> */}
+        <input type='text' placeholder='email' className={css.input}
+        onChange={(e) => setEmail(e.target.value)}/>
+        <input type='text' placeholder='password' className={css.input}
+        onChange={(e) => setPassword(e.target.value)}/>
+        <input type='submit' value='Register' className={css.input}/>
+      </form>
 
-export default function Registration() {
-  const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(3),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
-  }));
+    </div>
+  )
+};
 
-  const classes = useStyles();
+export default Registration;
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+
+// import React from 'react';
+// import Avatar from '@material-ui/core/Avatar';
+// import Button from '@material-ui/core/Button';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import TextField from '@material-ui/core/TextField';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
+// import Link from '@material-ui/core/Link';
+// import Grid from '@material-ui/core/Grid';
+// import Box from '@material-ui/core/Box';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+// import Typography from '@material-ui/core/Typography';
+// import { makeStyles, createTheme } from '@material-ui/core/styles';
+// import Container from '@material-ui/core/Container';
+
+// function Copyright() {
+//   return (
+//     <Typography variant="body2" color="textSecondary" align="center">
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://material-ui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
+
+// export default function Registration() {
+//   const useStyles = createTheme((theme) => ({
+//     paper: {
+//       marginTop: theme.spacing(8),
+//       display: 'flex',
+//       flexDirection: 'column',
+//       alignItems: 'center',
+//     },
+//     avatar: {
+//       margin: theme.spacing(1),
+//       backgroundColor: theme.palette.secondary.main,
+//     },
+//     form: {
+//       width: '100%', // Fix IE 11 issue.
+//       marginTop: theme.spacing(3),
+//     },
+//     submit: {
+//       margin: theme.spacing(3, 0, 2),
+//     },
+//   }));
+
+//   const classes = useStyles();
+
+//   return (
+//     <Container component="main" maxWidth="xs">
+//       <CssBaseline />
+//       <div className={classes.paper}>
+//         <Avatar className={classes.avatar}>
+//           <LockOutlinedIcon />
+//         </Avatar>
+//         <Typography component="h1" variant="h5">
+//           Sign up
+//         </Typography>
+//         <form className={classes.form} noValidate>
+//           <Grid container spacing={2}>
+//             <Grid item xs={12} sm={6}>
+//               <TextField
+//                 autoComplete="fname"
+//                 name="firstName"
+//                 variant="outlined"
+//                 required
+//                 fullWidth
+//                 id="firstName"
+//                 label="First Name"
+//                 autoFocus
+//               />
+//             </Grid>
+//             <Grid item xs={12} sm={6}>
+//               <TextField
+//                 variant="outlined"
+//                 required
+//                 fullWidth
+//                 id="lastName"
+//                 label="Last Name"
+//                 name="lastName"
+//                 autoComplete="lname"
+//               />
+//             </Grid>
+//             <Grid item xs={12}>
+//               <TextField
+//                 variant="outlined"
+//                 required
+//                 fullWidth
+//                 id="email"
+//                 label="Email Address"
+//                 name="email"
+//                 autoComplete="email"
+//               />
+//             </Grid>
+//             <Grid item xs={12}>
+//               <TextField
+//                 variant="outlined"
+//                 required
+//                 fullWidth
+//                 name="password"
+//                 label="Password"
+//                 type="password"
+//                 id="password"
+//                 autoComplete="current-password"
+//               />
+//             </Grid>
+//             <Grid item xs={12}>
+//               <FormControlLabel
+//                 control={<Checkbox value="allowExtraEmails" color="primary" />}
+//                 label="I want to receive inspiration, marketing promotions and updates via email."
+//               />
+//             </Grid>
+//           </Grid>
+//           <Button
+//             type="submit"
+//             fullWidth
+//             variant="contained"
+//             color="primary"
+//             className={classes.submit}
+//           >
+//             Sign Up
+//           </Button>
+//           <Grid container justifyContent="flex-end">
+//             <Grid item>
+//               <Link href="#" variant="body2">
+//                 Already have an account? Sign in
+//               </Link>
+//             </Grid>
+//           </Grid>
+//         </form>
+//       </div>
+//       <Box mt={5}>
+//         <Copyright />
+//       </Box>
+//     </Container>
+//   );
+// }
