3111c274ed64bdc62e90cecb50dcf044cbc63604
Brian <blee0613@gmail.com>
2021-08-28 11:53:31 -0700

    some Material UI added to blog

--- client/pages/blog/[blogID].js
+++ client/pages/blog/[blogID].js
@@ -1,14 +1,96 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { useRouter } from 'next/router';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import Typography from '@material-ui/core/Typography';
+import Hidden from '@material-ui/core/Hidden';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Card from '@material-ui/core/Card';
+
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+function FeaturedPost(props) {
+  const classes = useStyles();
+  const { post } = props;
+
+  return (
+    <Grid item xs={12} md={6}>
+      <CardActionArea component="a" href="#">
+        <Card className={classes.card}>
+          <div className={classes.cardDetails}>
+            <CardContent>
+              <Typography component="h2" variant="h5">
+                {post.title}
+              </Typography>
+              <Typography variant="subtitle1" color="textSecondary">
+                {post.date}
+              </Typography>
+              <Typography variant="subtitle1" paragraph>
+                {post.description}
+              </Typography>
+              <Typography variant="subtitle1" color="primary">
+                Continue reading...
+              </Typography>
+            </CardContent>
+          </div>
+          <Hidden xsDown>
+            <CardMedia className={classes.cardMedia} image={post.image} title={post.imageTitle} />
+          </Hidden>
+        </Card>
+      </CardActionArea>
+    </Grid>
+  );
+}
 
 export default function BlogPost() {
   const router = useRouter();
   const { blogID } = router.query;
+  const classes = useStyles();
+
+  const featuredPosts = [
+    {
+      title: 'Featured post',
+      date: 'Nov 12',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random',
+      imageText: 'Image Text',
+    },
+    {
+      title: 'Post title',
+      date: 'Nov 11',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random',
+      imageText: 'Image Text',
+    },
+  ];
+
   return (
-    <div>
-      this is blog #
-      {blogID}
-    </div>
+    <>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <main>
+          <Grid container spacing={4}>
+            {featuredPosts.map((post) => (
+              <FeaturedPost key={post.title} post={post} />
+            ))}
+          </Grid>
+        </main>
+      </Container>
+    </>
   );
 }
