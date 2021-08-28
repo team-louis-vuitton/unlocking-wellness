673f70b632a66fccfa4099592df549a9d77efdde
Caleb Broderick <liquidarkness1997@gmail.com>
2021-08-27 21:11:28 -0600

    card, results skeleton

---
+++ client/components/SearchCard.js
@@ -0,0 +1,14 @@
+import css from '../styles/SearchCard.module.css';
+
+const SearchCard = () => {
+  return (
+    <article>
+      <p>Info Piece 1</p>
+      <p>Info Piece 2</p>
+      <p>Info Piece 3</p>
+      <button>View Provider</button>
+    </article>
+  )
+}
+
+export default SearchCard;
--- client/pages/searchResults.js
+++ client/pages/searchResults.js
@@ -1,3 +1,16 @@
 // lists of physicians after search
 // need a way to pass down props
 // modal => physician information
+
+import SearchCard from '../components/SearchCard.js';
+
+const SearchResults = () => {
+  return (
+    <section>
+      <h2>Search Results</h2>
+      <SearchCard />
+    </section>
+  )
+}
+
+export default SearchResults;
