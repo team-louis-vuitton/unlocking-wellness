ad18ec82c831615dc21165af1fc560ba63342c00
Caleb Broderick <liquidarkness1997@gmail.com>
2021-08-28 12:12:26 -0600

    built static card skeleton

--- client/components/SearchCard.js
+++ client/components/SearchCard.js
@@ -1,14 +1,57 @@
-import css from '../styles/SearchCard.module.css';
+import styles from '../styles/SearchCard.module.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import mockData from '../searchCardMock.js';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 261,
+    maxHeight: 339,
+    borderRadius: 30,
+    backgroundColor: '#A3B3A9',
+    boxShadow: '4px 4px 10px rgba(0, 0, 0, 0.25)',
+  },
+  media: {
+    height: 140,
+  },
+});
 
 const SearchCard = () => {
+  const classes = useStyles();
+
   return (
-    <article>
-      <p>Info Piece 1</p>
-      <p>Info Piece 2</p>
-      <p>Info Piece 3</p>
-      <button>View Provider</button>
-    </article>
-  )
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={mockData[0].image_url}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {mockData[0].name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Category: {mockData[0].categories[0].title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Location: {mockData[0].location.city}, {mockData[0].location.state}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="large" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
 
 export default SearchCard;
--- client/searchCardMock.js
+++ client/searchCardMock.js
@@ -1,3 +1,230 @@
 export default [
-
+  {
+      'id': 'j45-I0KaQ4EgzYZ2q_G4kw',
+      'alias': 'palo-alto-medical-foundation-fremont',
+      'name': 'Palo Alto Medical Foundation',
+      'image_url': 'https://s3-media4.fl.yelpcdn.com/bphoto/BKplWElqPjYGDFEocmElbA/o.jpg',
+      'is_closed': false,
+      'url': 'https://www.yelp.com/biz/palo-alto-medical-foundation-fremont?adjust_creative=_AhrLdprWQOFnvndroR2zw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_AhrLdprWQOFnvndroR2zw',
+      'review_count': 339,
+      'categories': [
+          {
+              'alias': 'medcenters',
+              'title': 'Medical Centers'
+          }
+      ],
+      'rating': 2.5,
+      'coordinates': {
+          'latitude': 37.548619,
+          'longitude': -121.973907
+      },
+      'transactions': [],
+      'location': {
+          'address1': '3200 Kearney St',
+          'address2': '',
+          'address3': 'Fremont Center',
+          'city': 'Fremont',
+          'zip_code': '94538',
+          'country': 'US',
+          'state': 'CA',
+          'display_address': [
+              '3200 Kearney St',
+              'Fremont Center',
+              'Fremont, CA 94538'
+          ]
+      },
+      'phone': '+15104901222',
+      'display_phone': '(510) 490-1222',
+      'distance': 2223.199814440134
+  },
+  {
+    'id': 'j45-I0KaQ4EgzYZ2q_G4kw',
+    'alias': 'palo-alto-medical-foundation-fremont',
+    'name': 'Palo Alto Medical Foundation',
+    'image_url': 'https://s3-media4.fl.yelpcdn.com/bphoto/BKplWElqPjYGDFEocmElbA/o.jpg',
+    'is_closed': false,
+    'url': 'https://www.yelp.com/biz/palo-alto-medical-foundation-fremont?adjust_creative=_AhrLdprWQOFnvndroR2zw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_AhrLdprWQOFnvndroR2zw',
+    'review_count': 339,
+    'categories': [
+        {
+            'alias': 'medcenters',
+            'title': 'Medical Centers'
+        }
+    ],
+    'rating': 2.5,
+    'coordinates': {
+        'latitude': 37.548619,
+        'longitude': -121.973907
+    },
+    'transactions': [],
+    'location': {
+        'address1': '3200 Kearney St',
+        'address2': '',
+        'address3': 'Fremont Center',
+        'city': 'Fremont',
+        'zip_code': '94538',
+        'country': 'US',
+        'state': 'CA',
+        'display_address': [
+            '3200 Kearney St',
+            'Fremont Center',
+            'Fremont, CA 94538'
+        ]
+    },
+    'phone': '+15104901222',
+    'display_phone': '(510) 490-1222',
+    'distance': 2223.199814440134
+},
+{
+  'id': 'j45-I0KaQ4EgzYZ2q_G4kw',
+  'alias': 'palo-alto-medical-foundation-fremont',
+  'name': 'Palo Alto Medical Foundation',
+  'image_url': 'https://s3-media4.fl.yelpcdn.com/bphoto/BKplWElqPjYGDFEocmElbA/o.jpg',
+  'is_closed': false,
+  'url': 'https://www.yelp.com/biz/palo-alto-medical-foundation-fremont?adjust_creative=_AhrLdprWQOFnvndroR2zw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_AhrLdprWQOFnvndroR2zw',
+  'review_count': 339,
+  'categories': [
+      {
+          'alias': 'medcenters',
+          'title': 'Medical Centers'
+      }
+  ],
+  'rating': 2.5,
+  'coordinates': {
+      'latitude': 37.548619,
+      'longitude': -121.973907
+  },
+  'transactions': [],
+  'location': {
+      'address1': '3200 Kearney St',
+      'address2': '',
+      'address3': 'Fremont Center',
+      'city': 'Fremont',
+      'zip_code': '94538',
+      'country': 'US',
+      'state': 'CA',
+      'display_address': [
+          '3200 Kearney St',
+          'Fremont Center',
+          'Fremont, CA 94538'
+      ]
+  },
+  'phone': '+15104901222',
+  'display_phone': '(510) 490-1222',
+  'distance': 2223.199814440134
+},
+{
+  'id': 'j45-I0KaQ4EgzYZ2q_G4kw',
+  'alias': 'palo-alto-medical-foundation-fremont',
+  'name': 'Palo Alto Medical Foundation',
+  'image_url': 'https://s3-media4.fl.yelpcdn.com/bphoto/BKplWElqPjYGDFEocmElbA/o.jpg',
+  'is_closed': false,
+  'url': 'https://www.yelp.com/biz/palo-alto-medical-foundation-fremont?adjust_creative=_AhrLdprWQOFnvndroR2zw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_AhrLdprWQOFnvndroR2zw',
+  'review_count': 339,
+  'categories': [
+      {
+          'alias': 'medcenters',
+          'title': 'Medical Centers'
+      }
+  ],
+  'rating': 2.5,
+  'coordinates': {
+      'latitude': 37.548619,
+      'longitude': -121.973907
+  },
+  'transactions': [],
+  'location': {
+      'address1': '3200 Kearney St',
+      'address2': '',
+      'address3': 'Fremont Center',
+      'city': 'Fremont',
+      'zip_code': '94538',
+      'country': 'US',
+      'state': 'CA',
+      'display_address': [
+          '3200 Kearney St',
+          'Fremont Center',
+          'Fremont, CA 94538'
+      ]
+  },
+  'phone': '+15104901222',
+  'display_phone': '(510) 490-1222',
+  'distance': 2223.199814440134
+},
+{
+  'id': 'j45-I0KaQ4EgzYZ2q_G4kw',
+  'alias': 'palo-alto-medical-foundation-fremont',
+  'name': 'Palo Alto Medical Foundation',
+  'image_url': 'https://s3-media4.fl.yelpcdn.com/bphoto/BKplWElqPjYGDFEocmElbA/o.jpg',
+  'is_closed': false,
+  'url': 'https://www.yelp.com/biz/palo-alto-medical-foundation-fremont?adjust_creative=_AhrLdprWQOFnvndroR2zw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_AhrLdprWQOFnvndroR2zw',
+  'review_count': 339,
+  'categories': [
+      {
+          'alias': 'medcenters',
+          'title': 'Medical Centers'
+      }
+  ],
+  'rating': 2.5,
+  'coordinates': {
+      'latitude': 37.548619,
+      'longitude': -121.973907
+  },
+  'transactions': [],
+  'location': {
+      'address1': '3200 Kearney St',
+      'address2': '',
+      'address3': 'Fremont Center',
+      'city': 'Fremont',
+      'zip_code': '94538',
+      'country': 'US',
+      'state': 'CA',
+      'display_address': [
+          '3200 Kearney St',
+          'Fremont Center',
+          'Fremont, CA 94538'
+      ]
+  },
+  'phone': '+15104901222',
+  'display_phone': '(510) 490-1222',
+  'distance': 2223.199814440134
+},
+{
+  'id': 'j45-I0KaQ4EgzYZ2q_G4kw',
+  'alias': 'palo-alto-medical-foundation-fremont',
+  'name': 'Palo Alto Medical Foundation',
+  'image_url': 'https://s3-media4.fl.yelpcdn.com/bphoto/BKplWElqPjYGDFEocmElbA/o.jpg',
+  'is_closed': false,
+  'url': 'https://www.yelp.com/biz/palo-alto-medical-foundation-fremont?adjust_creative=_AhrLdprWQOFnvndroR2zw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=_AhrLdprWQOFnvndroR2zw',
+  'review_count': 339,
+  'categories': [
+      {
+          'alias': 'medcenters',
+          'title': 'Medical Centers'
+      }
+  ],
+  'rating': 2.5,
+  'coordinates': {
+      'latitude': 37.548619,
+      'longitude': -121.973907
+  },
+  'transactions': [],
+  'location': {
+      'address1': '3200 Kearney St',
+      'address2': '',
+      'address3': 'Fremont Center',
+      'city': 'Fremont',
+      'zip_code': '94538',
+      'country': 'US',
+      'state': 'CA',
+      'display_address': [
+          '3200 Kearney St',
+          'Fremont Center',
+          'Fremont, CA 94538'
+      ]
+  },
+  'phone': '+15104901222',
+  'display_phone': '(510) 490-1222',
+  'distance': 2223.199814440134
+}
 ]
