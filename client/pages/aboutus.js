97633f484abf5a10b4d962441dd5bf85b8222d05
Sophia Nguyen <nguyen.x.sophia@gmail.com>
2021-08-28 23:07:19 -0700

    Added photo for about us

--- client/pages/aboutus.js
+++ client/pages/aboutus.js
@@ -1,6 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+import styles from '../styles/AboutUs.module.css';
+
 export default function AboutUs() {
   return (
-    <div>
+    <div className={styles.page}>
       <div>
         This is AboutUs Page
         Lorem Ipsum
