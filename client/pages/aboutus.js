9ca3f92b3f20a231a5d90aa8a1580f7177819c58
Brian <blee0613@gmail.com>
2021-08-31 18:03:21 -0700

    fixed paragraph formatting

--- client/pages/aboutus.js
+++ client/pages/aboutus.js
@@ -21,53 +21,53 @@ export default function AboutUs() {
           <p className={styles.story}>Feugiat pretium nibh ipsum consequat nisl vel. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui. Nisl purus in mollis nunc sed id semper risus in. Nisi scelerisque eu ultrices vitae auctor eu. Etiam tempor orci eu lobortis elementum nibh tellus. Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. At ultrices mi tempus imperdiet nulla. Diam vel quam elementum pulvinar. Eros in cursus turpis massa tincidunt dui. Metus vulputate eu scelerisque felis.</p>
           <h2 id="mission" className={styles.missionTitle}>Our Mission</h2>
           <p className={styles.mission}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel eros donec ac odio tempor orci. Purus in massa tempor nec feugiat nisl pretium fusce. Pretium lectus quam id leo in vitae turpis massa sed. Leo in vitae turpis massa. Amet facilisis magna etiam tempor orci. Sed arcu non odio euismod lacinia at quis risus. Auctor eu augue ut lectus. Est lorem ipsum dolor sit amet consectetur. Posuere urna nec tincidunt praesent semper.</p>
-          <h2 id="the-team" className={styles.teamTitle}>Our Team</h2>
+          <h2 id="the-team" className={styles.missionTitle}>Our Team</h2>
           <div className={styles.teamContainer}>
             <div className={styles.teamItem}>
               <Avatar size="xl" name="Mx. Kay Oss" src="https://ca.slack-edge.com/T0455847Q-U01LPFZGJT0-0a8e0e904763-512" />
-              <p className="styles.teamName">Mx. Kay Oss</p>
-              <p className="styles.teamName">CEO</p>
+              <p className={styles.teamName}>Mx. Kay Oss</p>
+              <p className={styles.teamTitle}>CEO</p>
             </div>
             <div className={styles.teamItem}>
               <Avatar size="xl" name="Zadok Kim" src="https://ca.slack-edge.com/T0455847Q-U024Z3K3BHQ-d6e807a6725f-512" />
-              <p className="styles.teamName">Zadok Kim</p>
-              <p className="styles.teamName">PM</p>
+              <p className={styles.teamName}>Zadok Kim</p>
+              <p className={styles.teamTitle}>PM</p>
             </div>
             <div className={styles.teamItem}>
               <Avatar size="xl" name="Brian Lee" src="https://ca.slack-edge.com/T0455847Q-U024Z9S32AF-fbf79ee66503-512" />
-              <p className="styles.teamName">Brian Lee</p>
-              <p className="styles.teamName">AO</p>
+              <p className={styles.teamName}>Brian Lee</p>
+              <p className={styles.teamTitle}>AO</p>
             </div>
             <div className={styles.teamItem}>
               <Avatar size="xl" name="Sophia Nguyen" src="https://ca.slack-edge.com/T0455847Q-U024SA0H38W-3d16123f54ae-512" />
-              <p className="styles.teamName">Sophia Nguyen</p>
-              <p className="styles.teamName">UIO</p>
+              <p className={styles.teamName}>Sophia Nguyen</p>
+              <p className={styles.teamTitle}>UIO</p>
             </div>
             <div className={styles.teamItem}>
               <Avatar size="xl" name="Caleb Broderick" src="https://ca.slack-edge.com/T0455847Q-U025BMQGGKB-42a9e41029dc-512" />
-              <p className="styles.teamName">Caleb Broderick</p>
-              <p className="styles.teamName">VOR</p>
+              <p className={styles.teamName}>Caleb Broderick</p>
+              <p className={styles.teamTitle}>VOR</p>
             </div>
             <div className={styles.teamItem}>
               <Avatar size="xl" name="Keefe Dashiell
               " src="https://ca.slack-edge.com/T0455847Q-U024SA47PP0-b19625f23012-512" />
-              <p className="styles.teamName">Keefe Dashiell</p>
-              <p className="styles.teamName">TTM</p>
+              <p className={styles.teamName}>Keefe Dashiell</p>
+              <p className={styles.teamTitle}>TTM</p>
             </div>
             <div className={styles.teamItem}>
               <Avatar size="xl" name="Ran An" src="https://ca.slack-edge.com/T0455847Q-U025NT3MN00-d16835f6fd94-512" />
-              <p className="styles.teamName">Ran An</p>
-              <p className="styles.teamName">YAG</p>
+              <p className={styles.teamName}>Ran An</p>
+              <p className={styles.teamTitle}>YAG</p>
             </div>
             <div className={styles.teamItem}>
               <Avatar size="xl" name="Zachary Bretz" src="https://ca.slack-edge.com/T0455847Q-U025BMNAD4H-7f03f7e91b6b-512" />
-              <p className="styles.teamName">Zachary Bretz</p>
-              <p className="styles.teamName">LM</p>
+              <p className={styles.teamName}>Zachary Bretz</p>
+              <p className={styles.teamTitle}>LM</p>
             </div>
             <div className={styles.teamItem}>
               <Avatar size="xl" name="Ubin Jung" src="https://ca.slack-edge.com/T0455847Q-U02558CSYCC-607af5374092-512" />
-              <p className="styles.teamName">Ubin Jung</p>
-              <p className="styles.teamName">MMB</p>
+              <p className={styles.teamName}>Ubin Jung</p>
+              <p className={styles.teamTitle}>MMB</p>
             </div>
           </div>
           <h2 id="contact-us" className={styles.contactTitle}>Contact Us</h2>
