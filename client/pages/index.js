a46d43abc1e03a7f8d015421c7aa660edc99c029
Brian <blee0613@gmail.com>
2021-09-02 10:24:57 -0700

    chatbot button size

--- client/pages/index.js
+++ client/pages/index.js
@@ -53,9 +53,9 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className={styles.coverSearch}>
+        {/* <div className={styles.coverSearch}>
           <SearchMain />
-        </div>
+        </div> */}
         <br></br>
         <div className={styles.middleTitleContainer}>
           <h1 className={styles.middleTitle}>A Community of Holistic Health</h1>
@@ -80,7 +80,7 @@ export default function Home() {
         {/* <div className={styles.game}>
           <iframe src="http://wanted5games.com/games/html5/pow-new-en-s-iga-cloud/index.html?pub=10" name="cloudgames-com" width="970" height="540" frameBorder="0" scrolling="no"></iframe>
         </div> */}
-        <ChatBot />
+        {/* <ChatBot /> */}
         <Footer />
       </div>
     </div>
